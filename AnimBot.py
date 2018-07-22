d89a613d1403703a44e4564e8dabe47f6d2fb592
AdityaPutraS <adityaputra159@gmail.com>
2018-07-22 21:47:08 +0800

    Update data.json

--- AnimBot.py
+++ AnimBot.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from bs4 import BeautifulSoup
 import requests,webbrowser
 from selenium import webdriver
@@ -12,14 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from tkinter import *
 from tkinter import messagebox
 import json
-#url = 'https://drivenime.com/daftar-anime/'
-#response = requests.get(url)
-
-#soup = BeautifulSoup(response.content, "html.parser")
-
-
-# In[2]:
-
+import re
 
 def buka_link(url):
     response = requests.get(url)
@@ -40,10 +27,6 @@ def ambilLink(s):
             linkBagiLagi.append((a.get_text(),a.get('href')))
     return linkBagiLagi
 
-
-# In[ ]:
-
-
 def loadLink(link_):
     soup = buka_link('https://drivenime.com/daftar-anime/')
     temp = soup.findAll('a')
@@ -59,7 +42,7 @@ def loadLink(link_):
             if((a.get('href')=='#azindex-1')):
                 link.update({now : linknama_temp})
                 saveLink(link)
-                now = a.get_text() 
+                now = a.get_text()
                 linknama_temp = []
                 num = 0
             else:
@@ -83,9 +66,11 @@ def loadLink(link_):
                     break
                 num += 1
     return link
+
 def saveLink(link):
     with open('data.json', 'w') as outfile:
         json.dump(link, outfile)
+
 def reload(link_):
     link = loadLink(link_)
     saveLink(link)
@@ -96,6 +81,7 @@ def reload(link_):
             tmp += 1
     messagebox.showinfo("Reload", "Reload selesai")
     return link
+
 def openLink(link):
     tmp = 0
     for lin in link:
@@ -103,6 +89,7 @@ def openLink(link):
             list1.insert(tmp,li[2])
             tmp += 1
     return link
+
 def onselect(event,pilihan,listEps,idxEps,download):
     wid = event.widget
     nama = str(wid)
@@ -138,8 +125,7 @@ def onselect(event,pilihan,listEps,idxEps,download):
             index = int(liCur[0])
             idxHapus.clear()
             idxHapus.append(index)
-            #print('Link anime : ',linkAnime)
-        #print(idxHapus)
+
 def tambah(listEps,idxEps,download):
     if(idxEps):
         eps = listEps[idxEps[0]]
@@ -147,84 +133,79 @@ def tambah(listEps,idxEps,download):
         download.append(eps)
     else:
         messagebox.showwarning('Error','Pilih episode terlebih dahulu')
-        #print(download)
+
 def hapus(idxHapus):
-    #print(idxHapus)
     if(idxHapus):
         list3.delete(idxHapus[0])
         download.pop(idxHapus[0])
-        #print(download)
     else:
         messagebox.showwarning('Error','Pilih item yang mau dihapus terlebih dahulu')
 def gas(download):
     for i in download:
         skip_link(i[1])
-        #print(i[0],' Selesai')
-
-
-# In[ ]:
-
 
 #Variable Global
 pilihan,listEps,idxEps= [],[],[]
 download = []
 idxHapus = []
-#Kode Utama
-root = Tk()
-root.title('Anime Downloader')
-frame2 = Frame(root)
-frame2.pack(side = TOP)
-frame1 = Frame(root)
-frame1.pack(side = LEFT)
-frame3 = Frame(root)
-frame3.pack(side = LEFT)
-frame4 = Frame(root)
-frame4.pack(side = LEFT)
 
-with open('data.json') as data_file:
-    link = json.load(data_file)
 
-#Label
-label1 = Label(frame1, text = 'List Anime')
-label1.pack(side = TOP)
+if __name__ == "__main__":
+    #Kode Utama
+    root = Tk()
+    root.title('Anime Downloader')
+    frame2 = Frame(root)
+    frame2.pack(side = TOP)
+    frame1 = Frame(root)
+    frame1.pack(side = LEFT)
+    frame3 = Frame(root)
+    frame3.pack(side = LEFT)
+    frame4 = Frame(root)
+    frame4.pack(side = LEFT)
 
-label2 = Label(frame3, text = 'List Episode')
-label2.pack(side = TOP)
+    with open('data.json') as data_file:
+        link = json.load(data_file)
 
-label3 = Label(frame4, text = 'List Download')
-label3.pack(side = TOP)
-#Scrollbar
-scrollbar = Scrollbar(frame1)
-scrollbar.pack( side = RIGHT, fill = Y )
-list1 = Listbox(frame1,name = 'list1',width=70, height = 20,yscrollcommand = scrollbar.set, relief = FLAT)
-list1.bind('<<ListboxSelect>>',lambda event : onselect(event,pilihan,listEps,idxEps,download))
-list1.pack(side = TOP)
-scrollbar.config( command = list1.yview )
+    #Label
+    label1 = Label(frame1, text = 'List Anime')
+    label1.pack(side = TOP)
 
-scrollbar2 = Scrollbar(frame3)
-scrollbar2.pack(side = RIGHT,fill = Y)
-list2 = Listbox(frame3,name = 'list2',width = 70,height = 20,yscrollcommand = scrollbar2.set,relief=FLAT)
-list2.pack(side = TOP)
-scrollbar2.config( command = list2.yview )
-list2.bind('<<ListboxSelect>>',lambda event : onselect(event,pilihan,listEps,idxEps,idxHapus))
+    label2 = Label(frame3, text = 'List Episode')
+    label2.pack(side = TOP)
 
-scrollbar3 = Scrollbar(frame4)
-scrollbar3.pack(side = RIGHT,fill= Y)
-list3 = Listbox(frame4,name = 'list3',width = 70, height = 20,yscrollcommand = scrollbar3.set,relief=FLAT)
-list3.pack(side = TOP)
-scrollbar3.config(command = list3.yview)
-list3.bind('<<ListboxSelect>>',lambda event : onselect(event,pilihan,listEps,idxEps,idxHapus))
-    
-#Button
-but1 = Button(frame2,text = 'Load Database',command = lambda : openLink(link))
-but1.pack(side = LEFT)
-but2 = Button(frame2,text = 'Reload dari server',command =  lambda : reload(link))
-but2.pack(side = LEFT)
-but3 = Button(frame2,text = 'Tambah ke list Download', command = lambda : tambah(listEps,idxEps,download))
-but3.pack(side = LEFT)
-but4 = Button(frame2,text = 'Hapus dari list',command = lambda : hapus(idxHapus))
-but4.pack(side = LEFT)
-but5 = Button(frame2, text = 'Download',command = lambda : gas(download))#lol
-but5.pack(side = RIGHT)
-root.mainloop()
+    label3 = Label(frame4, text = 'List Download')
+    label3.pack(side = TOP)
+    #Scrollbar
+    scrollbar = Scrollbar(frame1)
+    scrollbar.pack( side = RIGHT, fill = Y )
+    list1 = Listbox(frame1,name = 'list1',width=70, height = 20,yscrollcommand = scrollbar.set, relief = FLAT)
+    list1.bind('<<ListboxSelect>>',lambda event : onselect(event,pilihan,listEps,idxEps,download))
+    list1.pack(side = TOP)
+    scrollbar.config( command = list1.yview )
 
+    scrollbar2 = Scrollbar(frame3)
+    scrollbar2.pack(side = RIGHT,fill = Y)
+    list2 = Listbox(frame3,name = 'list2',width = 70,height = 20,yscrollcommand = scrollbar2.set,relief=FLAT)
+    list2.pack(side = TOP)
+    scrollbar2.config( command = list2.yview )
+    list2.bind('<<ListboxSelect>>',lambda event : onselect(event,pilihan,listEps,idxEps,idxHapus))
+
+    scrollbar3 = Scrollbar(frame4)
+    scrollbar3.pack(side = RIGHT,fill= Y)
+    list3 = Listbox(frame4,name = 'list3',width = 70, height = 20,yscrollcommand = scrollbar3.set,relief=FLAT)
+    list3.pack(side = TOP)
+    scrollbar3.config(command = list3.yview)
+    list3.bind('<<ListboxSelect>>',lambda event : onselect(event,pilihan,listEps,idxEps,idxHapus))
+
+    #Button
+    but1 = Button(frame2,text = 'Load Database',command = lambda : openLink(link))
+    but1.pack(side = LEFT)
+    but2 = Button(frame2,text = 'Reload dari server',command =  lambda : reload(link))
+    but2.pack(side = LEFT)
+    but3 = Button(frame2,text = 'Tambah ke list Download', command = lambda : tambah(listEps,idxEps,download))
+    but3.pack(side = LEFT)
+    but4 = Button(frame2,text = 'Hapus dari list',command = lambda : hapus(idxHapus))
+    but4.pack(side = LEFT)
+    but5 = Button(frame2, text = 'Download',command = lambda : gas(download))#lol
+    but5.pack(side = RIGHT)
+    root.mainloop()
